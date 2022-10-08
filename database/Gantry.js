1bb593a6dba3cb694f2df13706a6f6e64c633f77
NoBugzzZ <739651338@qq.com>
2022-10-08 21:13:18 +0800

    优化count

--- database/Gantry.js
+++ database/Gantry.js
@@ -1,21 +1,24 @@
 const { MongoClient } = require("mongodb");
 const DB_URL = require("./config");
 
-const uri = DB_URL
+const uri = DB_URL;
 const client = new MongoClient(uri);
+let count = -1;
 
 async function find(skip = 0, limit = 200, query = {}) {
   if (!client.isConnected()) await client.connect();
-  const database = client.db('DigitalModel');
-  const gantry = database.collection('GantryDigital');
+  const database = client.db("DigitalModel");
+  const gantry = database.collection("GantryDigital");
   const cursor = gantry.find(query).limit(limit).skip(skip);
   const allValues = await cursor.toArray();
-  const count = await gantry.countDocuments(query);
-  return { items: allValues, skip: limit + skip, count }
+  if (count < 0) {
+    const count = await gantry.countDocuments(query);
+  }
+  return { items: allValues, skip: limit + skip, count };
 }
 
 // find(0, 200, {}).then(data => { console.log(data) })
 
 module.exports = {
   find,
-}
+};
--- database/Vehicle.js
+++ database/Vehicle.js
@@ -1,21 +1,30 @@
 const { MongoClient } = require("mongodb");
 const DB_URL = require("./config");
 
-const uri = DB_URL
+const uri = DB_URL;
 const client = new MongoClient(uri);
+let count = -1;
 
 async function find(skip = 0, limit = 200, query = {}) {
   if (!client.isConnected()) await client.connect();
-  const database = client.db('DigitalModel');
-  const vehicle = database.collection('VehicleDigital');
+  const database = client.db("DigitalModel");
+  const vehicle = database.collection("VehicleDigital");
+  // console.time("find")
   const cursor = vehicle.find(query).limit(limit).skip(skip);
+  // console.timeEnd("find");
+  // console.time("toArray")
   const allValues = await cursor.toArray();
-  const count = await vehicle.countDocuments(query);
-  return { items: allValues, skip: limit + skip, count }
+  // console.timeEnd("toArray")
+  // console.time("count");
+  if (count < 0) {
+    count = await vehicle.countDocuments(query);
+  }
+  // console.timeEnd("count");
+  return { items: allValues, skip: limit + skip, count };
 }
 
 // find(0, 200, {}).then(data => { console.log(data) })
 
 module.exports = {
   find,
-}
+};
