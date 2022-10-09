a09b247a7409e7d651f1531c2534571dfc987511
NoBugzzZ <739651338@qq.com>
2022-10-09 17:30:06 +0800

    优化count

--- database/TrafficTransaction.js
+++ database/TrafficTransaction.js
@@ -3,17 +3,36 @@ const DB_URL = require("./config");
 
 const uri = DB_URL
 const client = new MongoClient(uri);
-let count=-1;
+let count = -1;
 
 async function find(skip = 0, limit = 200, query = {}) {
+  // console.time("[TrafficTransaction] connect")
   if (!client.isConnected()) await client.connect();
+  // console.timeEnd("[TrafficTransaction] connect")
+
+  // console.time("[TrafficTransaction] collection")
   const database = client.db('EntityModel');
   const trafficTransaction = database.collection('TrafficTransaction');
+  // console.timeEnd("[TrafficTransaction] collection")
+
+  // console.time("[TrafficTransaction] find")
   const cursor = trafficTransaction.find(query).limit(limit).skip(skip);
+  // console.timeEnd("[TrafficTransaction] find")
+
+  // console.time("[TrafficTransaction] toArray")
   const allValues = await cursor.toArray();
-  if(count<0){
-    count = await trafficTransaction.countDocuments(query);
+  // console.timeEnd("[TrafficTransaction] toArray")
+
+  if (count < 0) {
+    // console.time("[TrafficTransaction] count")
+    trafficTransaction.countDocuments().then(c => {
+      count = c;
+    });
+    // console.timeEnd("[TrafficTransaction] count")
   }
+  // console.time("[TrafficTransaction] estimatedDocumentCount");
+  count = await trafficTransaction.estimatedDocumentCount();
+  // console.timeEnd("[TrafficTransaction] estimatedDocumentCount");
   return { items: allValues, skip: limit + skip, count }
 }
 
--- database/Vehicle.js
+++ database/Vehicle.js
@@ -6,20 +6,33 @@ const client = new MongoClient(uri);
 let count = -1;
 
 async function find(skip = 0, limit = 200, query = {}) {
+  console.time("[Vehicle] connect")
   if (!client.isConnected()) await client.connect();
+  console.timeEnd("[Vehicle] connect");
+
+  console.time("[Vehicle] collection")
   const database = client.db("DigitalModel");
   const vehicle = database.collection("VehicleDigital");
-  // console.time("find")
+  console.timeEnd("[Vehicle] collection")
+
+  console.time("[Vehicle] find")
   const cursor = vehicle.find(query).limit(limit).skip(skip);
-  // console.timeEnd("find");
-  // console.time("toArray")
+  console.timeEnd("[Vehicle] find");
+
+  console.time("[Vehicle] toArray")
   const allValues = await cursor.toArray();
-  // console.timeEnd("toArray")
-  // console.time("count");
+  console.timeEnd("[Vehicle] toArray")
+
   if (count < 0) {
-    count = await vehicle.countDocuments(query);
+    console.time("[Vehicle] count");
+    vehicle.countDocuments().then(c => {
+      count = c;
+    })
+    console.timeEnd("[Vehicle] count");
   }
-  // console.timeEnd("count");
+  console.time("[Vehicle] estimatedDocumentCount");
+  count = await vehicle.estimatedDocumentCount();
+  console.timeEnd("[Vehicle] estimatedDocumentCount");
   return { items: allValues, skip: limit + skip, count };
 }
 
